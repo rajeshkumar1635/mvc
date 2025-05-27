399db962055acc912c1f6a1957db0cce9a079090
rajeshkumar1635 <rajeshkumar.kurapati23@gmail.com>
2025-05-27 12:21:33 -0500

    initial commit for mvc

---
+++ Controllers/EngineeringController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc_app.Controllers
+{
+    public class EngineeringController : Controller
+    {
+        // GET: EngineeringController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
